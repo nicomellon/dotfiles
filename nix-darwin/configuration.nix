7140f6cdc370ebc56bafa0a96daa7473fa2de2a7
nicomellon <nicolas.mellon94@gmail.com>
2024-08-31 12:47:35 +0200

    refactor: stop using nix flakes

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -11,8 +11,6 @@
   services.nix-daemon.enable = true;
   services.skhd.enable = true;
 
-  nix.settings.experimental-features = "nix-command flakes";
-
   system.stateVersion = 4;
   system.defaults = {
     dock = {
@@ -23,10 +21,6 @@
       AppleShowAllExtensions = true;
       ShowPathbar = true;
       FXEnableExtensionChangeWarning = false;
-    };
-    NSGlobalDomain = {
-      AppleKeyboardUIMode = 3;
-      "com.apple.keyboard.fnState" = true;
     };
   };
 }
