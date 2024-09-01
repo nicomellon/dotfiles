9db7f319f45bf1a82d6bfda7014975827486e3ee
nicomellon <nmellon94@gmail.com>
2024-09-01 13:12:40 +0200

    feat: add git config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,19 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = "nm";
   home.homeDirectory = "/Users/nm";
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "24.05";
 
   home.file = {
@@ -25,6 +14,10 @@
     ".config/wezterm".source = ../wezterm;
     ".password-store".source = ../password-store;
   };
+
+  programs.git.enable = true;
+  programs.git.userEmail = "nmellon94@gmail.com";
+  programs.git.userName = "nicomellon";
 
   programs.gpg.enable = true;
   programs.gpg.publicKeys = [
