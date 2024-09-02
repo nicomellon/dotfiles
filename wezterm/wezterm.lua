c885bf50efa83e0738906bcca6444224aef5863e
nicomellon <nmellon94@gmail.com>
2024-09-02 10:40:38 +0200

    chore: tweak aerospace and wezterm configs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,37 +2,16 @@ local wezterm = require("wezterm")
 
 config = {
 	automatically_reload_config = true,
+	color_scheme = "Kanagawa (Gogh)",
 	enable_tab_bar = false,
+	initial_cols = 200,
+	initial_rows = 45,
+	font_size = 16,
+	window_background_opacity = 0.85,
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
 	window_padding = {
 		left = 30,
-	},
-	color_scheme = "Kanagawa (Gogh)",
-	initial_cols = 200,
-	initial_rows = 45,
-	font_size = 16,
-	background = {
-		{
-			source = {
-				File = "/Users/nm/dotfiles/pictures/nico.jpg",
-			},
-			hsb = {
-				hue = 1.0,
-				saturation = 1.02,
-				brightness = 0.05,
-			},
-			width = "100%",
-			height = "100%",
-		},
-		{
-			source = {
-				Color = "#282c35",
-			},
-			width = "100%",
-			height = "100%",
-			opacity = 0.55,
-		},
 	},
 }
 
