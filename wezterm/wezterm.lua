ab52bb8ffa209272130c36fe825df30bdb0379d4
Nico Mellon <nicolas.mellon@welltech.com>
2024-12-29 13:40:03 +0100

    remove opacity

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,18 +1,12 @@
-local wezterm = require("wezterm")
-
-config = {
+return {
 	automatically_reload_config = true,
 	color_scheme = "Kanagawa (Gogh)",
 	enable_tab_bar = false,
 	initial_cols = 200,
 	initial_rows = 45,
 	font_size = 16,
-	window_background_opacity = 0.85,
-	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
 	window_padding = {
 		left = 30,
 	},
 }
-
-return config
